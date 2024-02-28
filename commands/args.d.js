9088f51419e0aad08c205e33dcc3a22328ab0262
Tanner S <me@tannerschermerhorn.dev>
2024-02-28 09:52:53 -0500

    Add code and File

---
+++ commands/args.d.js
@@ -0,0 +1,7 @@
+import { APIApplicationCommandBasicOption } from 'discord.js';
+export declare class Args {
+    static readonly DEV_COMMAND: APIApplicationCommandBasicOption;
+    static readonly HELP_OPTION: APIApplicationCommandBasicOption;
+    static readonly INFO_OPTION: APIApplicationCommandBasicOption;
+}
+//# Pasted from Tanner s. Pterodactyl pannel
